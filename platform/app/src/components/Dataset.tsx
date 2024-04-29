9e1b34c2eb1266e7d6c0317bc561f286d7a74c6e
Alexandra Kláriková <alexandra.klarikova@student.tuke.sk>
2024-04-30 00:12:08 +0200

    dataset

--- platform/app/src/components/Dataset.tsx
+++ platform/app/src/components/Dataset.tsx
@@ -84,7 +84,6 @@ const Dataset: React.FC = () => {
           right: '80px', // Adjusted to create some space from the right
         }}
       >
-
       </div>
 
       {/* Main content, vertically and horizontally centered */}
@@ -103,6 +102,9 @@ const Dataset: React.FC = () => {
           {/* First clickable image */}
           <div
             style={{
+              position: 'absolute',
+              top: '290px',
+              right: '70px',
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
@@ -114,9 +116,55 @@ const Dataset: React.FC = () => {
             <img
               src="/assets/pictures/dset.png"
               alt="Image 1"
-              style={{ height: '300px', width: '300px' }}
+              style={{
+                  height: '300px',
+                  width: '300px'
+            }}
             />
-            <span style={{ marginTop: '50px', fontSize: '23px', color: 'white' }}>Dataset je pomenovaná ako RVENET, ktorá sa zaoberá identifikáciou dysfunkcie pravej komory. Obsahuej viac ako 3800 záznamov, ktoré sú v DICOM formáte a taktiež obsahuje aj kvalitné metadáta. Najčastejšie snímky sú zozbierané pomocou "apical 4 chamber view".</span>
+          </div>
+
+              <div style={{
+                  position: 'absolute',
+                  top: '170px',
+                  right: '150px', // Adjusted to create some space from the right
+                  left: '150px',
+                  color: 'white',
+              }}>
+                  <p>The dataset is named as RVENet (A Large Echocardiographic Dataset for the Deep Learning-Based Assessment of Right Ventricular Function)
+                      which deals with the identification of right ventricular dysfunction.
+                      The RVENet dataset was primarily designed to enable the training and evaluation of deep learning models that predict RVEF from 2D echocardiographic videos.
+                      The fact that each 2D video is labeled with a 3D echocardiography-derived RVEF value makes our dataset one of its kind. Beyond serving as a benchmark dataset in the task mentioned above, the RVENet dataset may represent a valuable resource for several other research projects in the intersection of computer vision and cardiovascular imaging.
+                  </p>
+
+                      <p>&nbsp;</p>
+
+                      <ul>
+                          <li>Two-dimensional (2D) echocardiography is the most frequently performed imaging test to assess right ventricular (RV) function.</li>
+                          <li>However, conventional 2D parameters are unable to reliably capture RV dysfunction across the entire spectrum of cardiac diseases.</li>
+                          <li>Three-dimensional (3D) echocardiography-derived RV ejection fraction (RVEF) – a sensitive and reproducible parameter that </li>
+                          <li>has been validated against cardiac magnetic resonance imaging – can bypass most of their limitations. Nonetheless, </li>
+                          <li>3D echocardiography has limited availability, is more time-consuming, and requires significant human expertise. Therefore, novel </li>
+                          <li>automated tools that utilize readily available and routinely acquired 2D echocardiographic recordings to predict RVEF and detect </li>
+                          <li>RV dysfunction reliably would be highly desirable. To enable the implementation of such innovative solutions, publicly available </li>
+                          <li>and sufficiently large dedicated datasets would be pivotal.</li>
+
+                      </ul>
+
+                  <p>&nbsp;</p>
+
+                  <ul>
+                      <li>The RVENet dataset consists of two major components: a large set of echocardiographic videos (in DICOM format) </li>
+                      <li>and the corresponding labels and additional patient or video-related data (in a single separate CSV file). It contains 3,583</li>
+                      <li> 2D apical four-chamber view echocardiographic videos from 944 examinations of 831 individuals in DICOM format. Each subject</li>
+                  </ul>
+                      <p> underwent one or more 3D transthoracic echocardiographic examinations between November 2013 and March 2021 at the Heart and Vascular Center of Semmelweis University.
+                      The dataset comprises ten distinct subgroups of subjects: healthy adult volunteers (n=192), healthy pediatric volunteers (n=54), elite athletes (n=139), patients with heart
+                      failure and reduced left ventricular EF (LVEF, n=98), patients with LV non-compaction cardiomyopathy (n=27), patients with aortic valve disease (n=85), patients with mitral valve disease (n=70),
+                      patients who underwent orthotopic heart transplantation (n=87),  pediatric patients who underwent kidney transplantation (n=23), and others (n=56).
+                     </p>
+
+
+
           </div>
         </div>
       </div>
